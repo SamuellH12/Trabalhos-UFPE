515114fb902e0186cc7416a4f507f4c39fef9126
SamuellH12 <84053584+SamuellH12@users.noreply.github.com>
2024-03-10 18:55:12 -0300

    dataset escolhido

--- estatistica/testeR.R
+++ estatistica/testeR.R
@@ -1,16 +1,33 @@
+# install.packages("shiny")
+# install.packages("shinydashboard")
+# install.packages("ggplot2")
+# install.packages("dplyr")
+
 library(shiny)
 library(shinydashboard)
 library(ggplot2)                # pro gráfico de linha
+library(dplyr)                  # pra filtrar os dados
 
 
 #------------------#
 #  INITIALIZATION  #  
 #------------------#
 
-data <- read.csv('df_stocks.csv')   # carrega o dataset
-listaDeClasses = unique(data$Index) # pega o nome das classes (os nomes estão me index nesse caso)
-min_time <- min(data$Date)
-max_time <- max(data$Date)
+data <- read.csv('owid-covid-data.csv')   # carrega o dataset
+
+listaDeClasses = c("Brazil", "European Union", "South Africa", "United States", "Australia", "China", "World", "Russia")
+data <- data %>% filter(location %in% listaDeClasses) #filtrar os dados para trabalhar com um dataset menor
+data <- data %>% filter(!if_any(new_cases_smoothed, is.na))
+data <- data %>% filter(!if_any(new_deaths_smoothed, is.na))
+listaDeClasses = unique(data$location) # pega o nome das classes (os nomes estão me region nesse caso)
+data$date <- as.Date(data$date)
+
+min_time <- min(data$date)
+max_time <- max(data$date)
+
+
+# Classes: $location
+# Dados: $new_cases_smoothed
 
 
 #------------------#
@@ -18,36 +35,49 @@ max_time <- max(data$Date)
 #------------------#
 
 ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
+  dashboardHeader(disable = TRUE), #disable pra simplificar
+  dashboardSidebar(disable = TRUE), # ^
   dashboardBody(
     
     # Inputs
     fluidRow( box( title="Selecione uma classe",width = 200,
-      # select class  
-      selectInput("Classe", label="Classe", choices=listaDeClasses, selected=listaDeClasses[0]),
-      # select date
-      sliderInput("DateRange", "Intervalo de tempo:", min = as.Date(min_time), max = as.Date(max_time),
-                  value=c(as.Date(min_time), as.Date(max_time)), timeFormat="%d/%m/%y" ),
-      # uiOutput("DateInputBox") #se quiser que as opções do input mudem de forma dinâmica
+                   # select class  
+                   selectInput("Classe", label="Classe", choices=listaDeClasses, selected="Brazil"),
+                   # select date
+                   sliderInput("DateRange", "Intervalo de tempo:", min = as.Date(min_time), max = as.Date(max_time),
+                               value=c(as.Date(min_time), as.Date(max_time)), timeFormat="%d/%m/%y" ),
     )),
     
     
     # plots      
     fluidRow(
       box(plotOutput("histo", height = 300)),
+      box(plotOutput("histo2", height = 300)),
       box(plotOutput("lineg", height = 300)),
-      box(plotOutput("boxpl", height = 300))
+      box(plotOutput("lineg2", height = 300)),
+      box(plotOutput("boxpl", height = 300)),
+      box(plotOutput("boxpl2", height = 300))
     ),
     
     # Medidas
     fluidRow(
+      titlePanel(h1("Medidas de Novos Casos por dia", style = "padding:0.25em; border:0.25em solid steelblue;")),
       valueBoxOutput("media"),
       valueBoxOutput("moda"),
       valueBoxOutput("mediana"),
       valueBoxOutput("desvio"),
       valueBoxOutput("valMax"),
       valueBoxOutput("valMin"),
+    ),
+    
+    fluidRow(
+      titlePanel(h1("Medidas de Novas Mortes por dia", style = "border-color:darkred; padding:0.25em; border:0.25em solid darkred;")),
+      valueBoxOutput("media2"),
+      valueBoxOutput("moda2"),
+      valueBoxOutput("mediana2"),
+      valueBoxOutput("desvio2"),
+      valueBoxOutput("valMax2"),
+      valueBoxOutput("valMin2")
     )
     
   )
@@ -67,49 +97,57 @@ server <- function(input, output) {
   max_time <- eventReactive(input$DateRange[2], {return (input$DateRange[2] ) })
   
   updatedData <- function(){
-    return (data[data$Index == (input$Classe) & data$Date >= min_time() & data$Date <= max_time(),])
+    return (data[data$location == (input$Classe) & data$date >= min_time() & data$date <= max_time(),])
   }
   
   #######  PLOTS  #######
   
-  output$histo <- renderPlot({  hist(updatedData()$Close )  })
+  output$histo <- renderPlot({ hist(updatedData()$new_cases_smoothed ,col="steelblue",xlab="Casos", main="Quantidade de Casos por dia") })
+  output$histo2<- renderPlot({ hist(updatedData()$new_deaths_smoothed,col="darkred",  xlab="Mortes",main="Quantidade de Mortes por dia")  })
   
   
   output$lineg <- renderPlot({
-    yValue = updatedData()$Close;
-    xValue = 1:length(yValue)
-    dataL = data.frame(xValue, yValue)
-    ggplot(dataL, aes(x=xValue, y=yValue)) + geom_line()
+    dataL = updatedData();
+    
+    ggplot(data=dataL, aes(x=date)) + 
+    geom_line(aes(y = dataL$new_cases_smoothed), color="steelblue") +
+    xlab("Tempo") + ylab("Novos Casos")
   })
   
-  
-  output$boxpl <- renderPlot({
-    yValue = updatedData()$Close;
-    xValue = 1:length(yValue)
-    dataL = data.frame(xValue, yValue)
-    ggplot(dataL, aes(x=xValue, y=yValue)) +  geom_boxplot(fill="blue", alpha=0.8) + xlab("sla")
+  output$lineg2 <- renderPlot({
+    dataL = updatedData();
+    
+    ggplot(data=dataL, aes(x=date)) + 
+    geom_line(aes(y = dataL$new_deaths_smoothed), color="darkred") +
+    xlab("Tempo") + ylab("Novas Mortes")
   })
+
+  
+  output$boxpl <- renderPlot({ boxplot(updatedData()$new_cases_smoothed, col="steelblue", main="Casos por dia")  })
+  output$boxpl2<- renderPlot({ boxplot(updatedData()$new_deaths_smoothed, col="darkred", main="Mortes por dia") })
   
   
+  Mode <- function(x) {
+    ux <- unique(x)
+    ux[which.max(tabulate(match(x, ux)))]
+  }
   
   #######  Medidas  #######
   
-  output$media <- renderValueBox({ valueBox(round(mean(updatedData()$Close),2), "Média",     icon=icon("chart-bar"),       color="orange") })
-  output$moda  <- renderValueBox({ valueBox(     (mode(updatedData()$Close)  ), "Moda",      icon=icon("chess-queen"),     color="purple") })
-  output$mediana<-renderValueBox({ valueBox(round(median(updatedData()$Close),2),"Mediana",  icon=icon("star-half-stroke"),color="yellow") })
-  output$desvio<- renderValueBox({ valueBox(round(sd(updatedData()$Close),2),"Desvio Padrão",icon=icon("newspaper"),       color="blue") })
-  output$valMax<- renderValueBox({ valueBox(round(max(updatedData()$Close),2),"Valor Máximo",icon=icon("circle-up"),       color="green") })
-  output$valMin<- renderValueBox({ valueBox(round(min(updatedData()$Close),2),"Valor Mínimo",icon=icon("circle-down"),     color="red") })
+  output$media <- renderValueBox({ valueBox(round(mean(updatedData()$new_cases_smoothed),2), "Média",     icon=icon("chart-bar"),       color="orange") })
+  output$moda  <- renderValueBox({ valueBox(round(Mode(updatedData()$new_cases_smoothed), 2), "Moda",      icon=icon("chess-queen"),     color="purple") })
+  output$mediana<-renderValueBox({ valueBox(round(median(updatedData()$new_cases_smoothed),2),"Mediana",  icon=icon("star-half-stroke"),color="yellow") })
+  output$desvio<- renderValueBox({ valueBox(round(sd(updatedData()$new_cases_smoothed),2),"Desvio Padrão",icon=icon("newspaper"),       color="blue") })
+  output$valMax<- renderValueBox({ valueBox(round(max(updatedData()$new_cases_smoothed),2),"Valor Máximo",icon=icon("circle-up"),       color="green") })
+  output$valMin<- renderValueBox({ valueBox(round(min(updatedData()$new_cases_smoothed),2),"Valor Mínimo",icon=icon("circle-down"),     color="red") })
   
-  
-#  output$DateInputBox <- renderUI({ #se quiser que as opções do input date mudem de forma dinâmica
-#    dataFilter = data[data$Index == (input$Classe),]
-#    mn = as.Date(min(dataFilter$Date))
-#    mx = as.Date(max(dataFilter$Date))
-#    
-#    sliderInput("DateRange", "Intervalo de tempo:", min=mn, max=mx, value=c(mn, mx), timeFormat="%d/%m/%y" )
-#  })
-  
+  output$media2 <- renderValueBox({ valueBox(round(mean(updatedData()$new_deaths_smoothed),2), "Média",     icon=icon("chart-bar"),       color="orange") })
+  output$moda2  <- renderValueBox({ valueBox(round(Mode(updatedData()$new_deaths_smoothed), 2), "Moda",      icon=icon("chess-queen"),     color="purple") })
+  output$mediana2<-renderValueBox({ valueBox(round(median(updatedData()$new_deaths_smoothed),2),"Mediana",  icon=icon("star-half-stroke"),color="yellow") })
+  output$desvio2<- renderValueBox({ valueBox(round(sd(updatedData()$new_deaths_smoothed),2),"Desvio Padrão",icon=icon("newspaper"),       color="blue") })
+  output$valMax2<- renderValueBox({ valueBox(round(max(updatedData()$new_deaths_smoothed),2),"Valor Máximo",icon=icon("circle-up"),       color="green") })
+  output$valMin2<- renderValueBox({ valueBox(round(min(updatedData()$new_deaths_smoothed),2),"Valor Mínimo",icon=icon("circle-down"),     color="red") })
+
 }
 
 
